115407b0d709bcff3feb1ffc329e5ea812b8b221
lawrence <lawrence.t.yen@gmail.com>
2019-07-24 16:34:07 -0700

    finalized

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
-router.post('wall/:wallId/comment/create', commentController.create);
+router.post('/wall/:wallId/comment/create', commentController.create);
 
 module.exports = router;
